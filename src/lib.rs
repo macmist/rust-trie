26d5c5ed075b875a38537890f5e4dd22ae155f42
Quentin MUSY <quentin.musy@gmail.com>
2025-02-24 16:44:31 +0000

    chore: doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,9 @@
+//! Easy implementation of tries in rust.
+//! 
+//! A simple crate to use tries in rust.
+//! At the moment, supports only insertion and checking if a word is in the trie.
+//! Only works with `str` types.
+ 
+
 pub mod trie;
 pub mod trie_node;
